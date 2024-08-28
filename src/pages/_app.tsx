b6d0036ed267b40e7108b20ba0eba4622c70bcf1
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-28 15:15:49 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "@/component/Context";
+// import { Provider } from "@/component/Context";
+import { Provider } from "react-redux";
+import { store } from "@/component/store/Store";
 export default function App({ Component, pageProps }: AppProps) {
   return(
 <>
-<Provider>
+{/* <Provider> for context api*/}
+<Provider store={store}>
 <Component {...pageProps} />
 </Provider>
+{/* </Provider> for context api*/}
 </>
   );
 }
